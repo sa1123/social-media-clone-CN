5ba2546e86e1e71fe78003abe1e58d7017c38183
Sahil <51375919+sa1123@users.noreply.github.com>
2023-04-18 15:04:24 +0530

    posts frontend

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+import React from "react";
+import {connect} from 'react-redux';
+
+import { fetchPosts } from "../actions/posts";
+import {PostsList} from './';
+
+class App extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+
+  render(){
+    const {posts} = this.props;
+    return (
+      <div>
+        <PostsList posts={posts}/>
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state){
+  return {
+    posts: state.posts,
+  };
+}
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import { configureStore } from './store';
+import {Provider} from 'react-redux';
 
 const store = configureStore();
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
