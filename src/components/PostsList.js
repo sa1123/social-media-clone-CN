377639717ab0072b6926492dbd352774b7961c60
Sahil <51375919+sa1123@users.noreply.github.com>
2023-04-18 16:28:22 +0530

    navbar

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,4 +1,5 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
 
 class PostsList extends Component {
     render() {
@@ -50,6 +51,10 @@ class PostsList extends Component {
             </div>
         )
     }
+}
+
+PostsList.propTypes = {
+    posts: PropTypes.array.isRequired,
 }
 
 export default PostsList;
